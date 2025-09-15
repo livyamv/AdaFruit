b38e4734d38281baf44cb8b0cdd02988d89e7174
livyamv <liviareis2702@gmail.com>
2025-09-15 11:32:22 -0300

    distancia

--- Temperatura18_08/Temperatura18_08.ino
+++ Temperatura18_08/Temperatura18_08.ino
@@ -1,23 +1,23 @@
-#include <math.h>
-#include "AdafruitIO_WiFi.h"
-#include "NewPing.h"
+// #include <math.h>
+// #include "AdafruitIO_WiFi.h"
+// #include "NewPing.h"
 
-//configurações da rede Wifi
-#define WIFI_SSID "malu"
-#define WIFI_PASS "13131313"
+// //configurações da rede Wifi
+// #define WIFI_SSID "malu"
+// #define WIFI_PASS "13131313"
 
-//Autenticação Adafruit IO
-#define IO_USERNAME "livmaria"
-#define IO_KEY "aio_gXYa52pp1ADvBXYnsCkeKac0fBBu"
+// //Autenticação Adafruit IO
+// #define IO_USERNAME "livmaria"
+// #define IO_KEY ""
 
-//AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+// AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
 #define pinNTC 34
-#define pinLed 14 // Pino do LED
-#define BUZZER_PIN 27 //Pino da buzzer
-#define LED_ALARME 13 
+#define pinLed 14      // Pino do LED
+#define BUZZER_PIN 27  //Pino da buzzer
+#define LED_ALARME 13
 #define BOTAO_FISICO 26
-#define TRIG_PIN 12 
+#define TRIG_PIN 12
 #define ECHO_PIN 14
 
 //Configuração do ultrassônico
@@ -31,11 +31,20 @@ float temp_anterior = -1;
 //Variável / ponteiro para referenciar o feed temperatura
 //AdafruitIO_Feed *botaoled = io.feed("botaoled");
 
-const float Rfixo = 10000.0; //Resistor do projeto
+const float Rfixo = 10000.0;  //Resistor do projeto
 const float Beta = 3950.0;
-const float R0 = 10000; // Nominal do ntc
-const float T0_kelvin = 298.15; // 25°C em Kelvin
+const float R0 = 10000;          // Nominal do ntc
+const float T0_kelvin = 298.15;  // 25°C em Kelvin
 const float Vcc = 3.3;
+
+//Definição dos Feeds
+AdafruitIO_Feed *botaoAlarme = io.feed("botaoalarme");
+
+//Definição Variáveis de controle
+bool alarmeAtivo = false;
+unsigned int distancia = 0;
+const int LIMITE_DISTANCIA = 15;
+
 
 void setup() {
   pinMode(pinNTC, INPUT);
@@ -49,40 +58,64 @@ void setup() {
 
   Serial.begin(115200);
 
-  while(!Serial);
+  while (!Serial)
+    ;
 
-  // Serial.print("Conectando ao Adafruit IO");
-  // io.connect();
+  Serial.print("Conectando ao Adafruit IO");
+  io.connect();
 
-  // while(io.status() < AIO_CONNECTED){
-  //   Serial.print(".");
-  //   delay(500);
-  // }
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
 
   // Serial.println();
   // Serial.println(io.statusText());
 
   // Configuração do callback, quando o feed receber(atualizar) um valor
   // botaoled -> onMessage(handleBotaoLed);
-  // Registra a função de callback 
+  // Registra a função de callback
   // Ela será chamada sempre que o feed receber um novo dado
+
+
+  // Vincula a função callback ao feed
+  botaoAlarme->onMessage(handleAlarme);
+
+  Serial.println("Solicitação o estado inicial do alarme: ");
+  botaoAlarme->get();
 
   delay(1000);
 }
 
 void loop() {
-  Serial.print(F("Distacia Lida: "));
-  Serial.println(sonar.ping_cm());
-  delay(500);
-  // //Manter a conexão com o Adafruit IO ativa
-  // io.run();
-  
+  io.run();
+
+  //Leitura do botão físico
+  if (digitalRead(BOTAO_FISICO) == 1) {
+    delay(200);  //debouce simples
+    alarmeAtivo = !alarmeAtivo;
+
+    botaoAlarme->save(String(alarmeAtivo ? "true" : "false"));
+    Serial.println(alarmeAtivo ? F("Alarme ARMADO pelo botao físico.") : F("Alarme DESARMADO pelo botao físico."));
+  }
+
+  distancia = sonar.ping_cm();
+  Serial.print("Distancia lida: ");
+  Serial.println(distancia);
+  Serial.println("cm");
+
+    //Ativação pu desativaçao do alarme
+    if (alarmeAtivo && distancia > 0 && distancia < LIMITE_DISTANCIA) {
+    ativarAlarme();
+  }
+
+  else {
+    desativarAlarme();
+  }
+
   // //publicacao(); // Chamada da função publish
-
   // delay(500);
-
   // testeBuzzer();
   // testeLed();
-  testeBotao(BOTAO_FISICO);
-
+  // testeBotao(BOTAO_FISICO);
 }
---
+++ Temperatura18_08/alarme.ino
@@ -0,0 +1,23 @@
+void handleAlarme(AdafruitIO_Data *data) {
+  String valor = data->toString();
+
+  Serial.print(F("Valor recebido do feed: "));
+  Serial.println(valor);
+
+  if (valor == "true") {
+    alarmeAtivo = true;
+    Serial.println("Alarme ARMADO pelo dash / app!");
+  } else {
+    alarmeAtivo = true;
+    Serial.println("Alarme DESARMADO pelo dash / app!");
+  }
+}
+void ativarAlarme() {
+  pinMode(BUZZER_PIN, HIGH);
+  pinMode(LED_ALARME, HIGH);
+}
+
+void desativarAlarme() {
+  pinMode(BUZZER_PIN, LOW);
+  pinMode(LED_ALARME, LOW);
+}
